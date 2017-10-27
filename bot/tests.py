80e8f36cba80f0e799ec64bc938be947ed47c36b
Brian Lee <briansmlee@gmail.com>
2017-10-27 01:37:50 -0400

    testing with travis

--- bot/tests.py
+++ bot/tests.py
@@ -2,29 +2,34 @@ import unittest
 from bot import Bot
 
 class TestAptBot(unittest.TestCase):
-    self.bot = Bot()
     def test_help(self):
-        length = self.bot.handle_command('help')
+        bot = Bot()
+        length = bot.handle_command('help')
         self.assertEqual(length, 0)
 
     def test_invalid(self):
-        length = self.bot.handle_command('group')
+        bot = Bot()
+        length = bot.handle_command('group')
         self.assertEqual(length, 0)
 
     def test_group(self):
-        length = self.bot.handle_command('group APT 2')
+        bot = Bot()
+        length = bot.handle_command('group APT 2')
         self.assertEqual(length, 4)
 
     def test_tool(self):
-        length = self.bot.handle_command('tool backdoor')
+        bot = Bot()
+        length = bot.handle_command('tool backdoor')
         self.assertEqual(length, 8)
 
     def test_target(self):
-        length = self.bot.handle_command('target japan')
+        bot = Bot()
+        length = bot.handle_command('target japan')
         self.assertEqual(length, 4)
 
     def test_ops(self):
-        length = self.bot.handle_command('ops desert')
+        bot = Bot()
+        length = bot.handle_command('ops desert')
         self.assertEqual(length, 1)
 
 if __name__ == '__main__':
